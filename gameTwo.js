4b1644e030ac3268c92be99118055bd07e88dbad
andreilarsen <42559253+andreilarsen@users.noreply.github.com>
2018-09-25 12:52:40 -0700

    Update gameTwo.js

--- gameTwo.js
+++ gameTwo.js
@@ -1,1 +1,15 @@
+window.addEventListener("load", whenPageIsLoaded, false);
 
+function whenPageIsLoaded() {
+  console.log("Successfully loaded page.");
+  canvas2();
+}
+
+function canvas2() {
+  var canvas = document.getElementById("canvas2");
+  if(!canvas) return;
+  var canvasWidth = canvas.width;
+  var canvasHeight = canvas.height;
+  var context = canvas.getContext("2d");
+  console.log("The width is " + canvasWidth + " and the height is " + canvasHeight ".");
+}

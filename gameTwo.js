4f0707057cf69dd8ec118801306e954d4642bf07
andreilarsen <42559253+andreilarsen@users.noreply.github.com>
2018-09-25 13:59:54 -0700

    Update gameTwo.js

--- gameTwo.js
+++ gameTwo.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", whenPageIsLoaded, false);
 
 function whenPageIsLoaded() {
-  console.log("Successfully loaded The page.");
+  console.log("Successfully loaded the page.");
   canvas2();
 }
 

df59ac97ca49699da156b2d2f64fd0eb0f03ddb6
andreilarsen <42559253+andreilarsen@users.noreply.github.com>
2018-09-25 13:18:37 -0700

    Update gameTwo.js

--- gameTwo.js
+++ gameTwo.js
@@ -11,15 +11,36 @@ function canvas2() {
   var context = canvas.getContext("2d");
   console.log("The width is " + canvas.width + " and the height is " + canvas.height + ".");
   var gameOver = false;
+  var up = 0;
+  var down = 0;
+  var left = 0;
+  var right = 0;
   
   function initGame() {
     gameOver = false;
+    window.addEventListener("keydown", keyWasPressed, true);
     drawCanvas();
   }
   
-  function drawCanvas() {
+  function keyWasPressed(e) {
+    console.log("Key was pressed");
+  }
+  
+  function drawBackground() {
     context.fillStyle = "#00ff00";
     context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+  
+  function drawPlayer(x, y) {
+    context.fillStyle = "#ffff00";
+    context.arc(x, y, 75, 0, 2*Math.PI, false);
+  }
+  
+  function drawCanvas() {
+    drawBackground();
+    while(!gameOver) {
+      drawPlayer(50, 50);
+    }
   }
   
   initGame();
